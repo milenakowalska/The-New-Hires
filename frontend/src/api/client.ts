d9a065c1d4f5b3972cbf8becbc9ebbf935a69230
Milena Kowalska <milenakowalska126@gmail.com>
2025-12-06 16:28:16 +0100

    change env var

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: `${import.meta.env.BASE_API_URL}/api`, // Backend URL
+    baseURL: `${import.meta.env.VITE_BASE_API_URL}/api`, // Backend URL
 });
 
 api.interceptors.request.use((config) => {
